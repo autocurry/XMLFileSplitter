10caccd9f9969f00fb29b0c3e25d10d37bc65ea0
autocurry <sandeepsekhar555@gmail.com>
2020-04-03 16:43:57 +0530

    done

---
+++ XMLSplitterEngine.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+root=tk.Tk()
+root.title("Save the - INVESTOMAN")
+
+baseframe = tk.Frame(root,height=200,width=400)
+baseframe.pack()
+
+root.mainloop()
--- xmlsplitter.py
+++ xmlsplitter.py
@@ -1,15 +1,16 @@
 import xml.etree.ElementTree as ET
-context = ET.iterparse('output.xml', events=('end', ))
-title = 'splittedfile'
-filename = format(title + ".xml")
-with open(filename, 'wb') as f:
-    f.write(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-    f.write(b"<root>\n")
-    count = 10
-    for event, elem in context:
-        count -= 1
-        if elem.tag == 'p' and count >= 0:
-            f.write(ET.tostring(elem)) 
-    f.write(b"</root>\n")
-        
+
+def split():
+    context = ET.iterparse('output.xml', events=('end', ))
+    title = 'splittedfile'
+    filename = format(title + ".xml")
+    with open(filename, 'wb') as f:
+        f.write(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+        f.write(b"<root>\n")
+        count = 10
+        for elem in context:
+            count -= 1
+            if elem.tag == 'p' and count >= 0:
+                f.write(ET.tostring(elem)) 
+        f.write(b"</root>\n")      
         
