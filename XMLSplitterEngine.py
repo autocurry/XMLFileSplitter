33a90bd220c92eb0a3ba75d054f1b1adbeb67098
autocurry <sandeepsekhar555@gmail.com>
2020-04-04 13:29:12 +0530

    completed creating the xml file splitter

--- XMLSplitterEngine.py
+++ XMLSplitterEngine.py
@@ -1,9 +1,62 @@
 import tkinter as tk
+from tkinter import *
+from tkinter import filedialog
+import os
+from xmlsplitter import *
+from tkinter import messagebox
+import logging
+
+logging.basicConfig(level=logging.DEBUG, filename='app.log', filemode='w', format='%(asctime)s - %(message)s')
+
+filepath = ''
+def browse():
+    global filepath
+    filepath = filedialog.askopenfilename(
+    title="Choose directory",
+    initialdir=os.getcwd(),
+    filetypes=[("XML Files", "*.xml")]        
+    )        
+
+def split():
+    if(filepath != NONE and filepath != ''):
+        count = int(countbox.get())
+        splittag=str(splittertag.get())
+        logging.info(f'splitting the file: {filepath} with {count} records on {splittag} tag')
+        splitxmlfile(filepath,count,splittag)
+        messagebox.showinfo('Success','File generated')
+    else:
+        messagebox.showinfo('Warning','Please select xml file to split')
+    
 
 root=tk.Tk()
-root.title("Save the - INVESTOMAN")
+root.title("XML File Splitter")
 
-baseframe = tk.Frame(root,height=200,width=400)
+baseframe = tk.Frame(root,height=300,width=400, borderwidth=5)
 baseframe.pack()
+
+totalframe = tk.Frame(baseframe, bg='#ffffff')
+totalframe.place(relx=0.45,rely=0.2,relwidth=0.9,relheight=0.7,anchor='n')
+
+browselabel = tk.Label(totalframe, text="Please select the XML File",fg="Black",font=('Helvetica','12'),bg="white")
+browselabel.grid(column=0,row=0, padx=5,pady=5)
+
+browsebutton = tk.Button(totalframe,text='Browse', command=browse,fg="Black",font=('Helvetica','12'))
+browsebutton.grid(column=1,row=0,padx=5,pady=5)
+
+countlabel = tk.Label(totalframe, text="Count of records",fg="Black",font=('Helvetica','12'),bg="white")
+countlabel.grid(column=0,row=1, padx=5,pady=5)
+
+countbox = tk.Entry(totalframe,fg="Black",font=('Helvetica','12'),bg="white",width=5)
+countbox.grid(column=1,row=1, padx=5,pady=5)
+
+splitterlabel = tk.Label(totalframe, text="Splitter Tag",fg="Black",font=('Helvetica','12'),bg="white")
+splitterlabel.grid(column=0,row=2, padx=5,pady=5)
+
+splittertag = tk.Entry(totalframe,fg="Black",font=('Helvetica','12'),bg="white",width=5)
+splittertag.insert(END,'p')
+splittertag.grid(column=1,row=2, padx=5,pady=5)
+
+convertbutton = tk.Button(totalframe,text='Split', command=split, relief=RAISED,fg="Black",font=('Helvetica','14','bold'))
+convertbutton.grid(column=0,row=4,padx=25,pady=5)
 
 root.mainloop()
--- xmlsplitter.py
+++ xmlsplitter.py
@@ -1,16 +1,22 @@
 import xml.etree.ElementTree as ET
+import sys
+import logging
 
-def split():
-    context = ET.iterparse('output.xml', events=('end', ))
-    title = 'splittedfile'
-    filename = format(title + ".xml")
-    with open(filename, 'wb') as f:
-        f.write(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
-        f.write(b"<root>\n")
-        count = 10
-        for elem in context:
-            count -= 1
-            if elem.tag == 'p' and count >= 0:
-                f.write(ET.tostring(elem)) 
-        f.write(b"</root>\n")      
+def splitxmlfile(filepath,count,tag):
+    try:
+        logging.info('Splitting in progress')
+        context = ET.iterparse(filepath, events=('end', ))
+        title = 'splittedfile'
+        filename = format(title + ".xml")
+        with open(filename, 'wb') as f:
+            f.write(b"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+            f.write(b"<root>\n")      
+            for event, elem in context:
+                count -= 1
+                if elem.tag == tag and count >= 0:
+                    f.write(ET.tostring(elem)) 
+            f.write(b"</root>\n")  
+    except IOError:
+        type, value, traceback = sys.exc_info()
+        logging.error('Error opening %s: %s' % (value.filename, value.strerror))   
         
