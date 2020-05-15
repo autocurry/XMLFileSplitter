8099099bec08b8372474aded7009251877031968
autocurry <sandeepsekhar555@gmail.com>
2020-05-15 13:01:33 +0530

    trim also completed, search and trim pending

--- XMLSplitterEngine.py
+++ XMLSplitterEngine.py
@@ -67,25 +67,6 @@ def split():
         messagebox.showinfo('Warning',messages)
 
 
-# trim function splits the whole xml file into child files having 10,000 records of the tag mentioned
-def trim():
-    #validate
-    messagelist=[]
-    if(browsefilename.get() == ''):
-        messagelist.append(validations[0])
-    if(splittertag.get()== ''):
-        messagelist.append(validations[2])
-
-    if(len(messagelist)== 0):        
-        splittag=str(splittertag.get())
-        outpath = outputflename.get()
-        splitxmlfilewithcounter(filepath,splittag,outpath)
-        messagebox.showinfo('Success',f'Files generated in the path: {os.getcwd()+"/Output"+"/"}')
-    else:
-        messages = ''
-        for message in messagelist:
-            messages = messages + message +"\n"
-        messagebox.showinfo('Warning',messages)
 
 def searchandtrim():      
     messagelist=[]
@@ -145,45 +126,125 @@ tabmenu.pack(expand=1, fill='both', padx=5, pady=5)
 
 
 #SplitTab controls 
-totalframe = tk.Frame(splittab, bg='#FFFFFF')
-totalframe.place(relx=0.5,rely=0.1,relwidth=0.9,relheight=0.7,anchor='n')
 
-browselabel = tk.Label(totalframe, text="Please select the XML File *",fg="Black",font=('Helvetica','12'),bg="white")
+browselabel = tk.Label(splittab, text="Please select the XML File *",fg="Black",font=('Helvetica','12'),bg="white")
 browselabel.grid(column=0,row=0, padx=5,pady=5,sticky = W)
 
-browsefilename = tk.Entry(totalframe,fg="Black",font=('Helvetica','12'),bg="white",width=20)
+browsefilename = tk.Entry(splittab,fg="Black",font=('Helvetica','12'),bg="white",width=20)
 browsefilename.grid(column=1,row=0, padx=5,pady=5)
 
-browsebutton = tk.Button(totalframe,text='Browse', command=browse,fg="Black",font=('Helvetica','12'))
+browsebutton = tk.Button(splittab,text='Browse', command=browse,fg="Black",font=('Helvetica','12'))
 browsebutton.grid(column=2,row=0,padx=5,pady=5)
 
-countlabel = tk.Label(totalframe, text="Count of records", fg="Black",font=('Helvetica','12'),bg="white")
+countlabel = tk.Label(splittab, text="Count of records", fg="Black",font=('Helvetica','12'),bg="white")
 countlabel.grid(column=0,row=1, padx=5,pady=5,sticky = W)
 
-countbox = tk.Entry(totalframe,fg="Black",font=('Helvetica','12'),bg="white",width=5)
+countbox = tk.Entry(splittab,fg="Black",font=('Helvetica','12'),bg="white",width=5)
 countbox.insert(END,'5')
 countbox.grid(column=1,row=1, padx=5,pady=5,sticky = W)
 
 
-outputbrowselabel = tk.Label(totalframe, text="Please select the output filename",fg="Black",font=('Helvetica','12'),bg="white")
+outputbrowselabel = tk.Label(splittab, text="Please select the output filename",fg="Black",font=('Helvetica','12'),bg="white")
 outputbrowselabel.grid(column=0,row=2, padx=5,pady=5,sticky = W)
 
-outputflename = tk.Entry(totalframe,fg="Black",font=('Helvetica','12'),bg="white",width=20)
+outputflename = tk.Entry(splittab,fg="Black",font=('Helvetica','12'),bg="white",width=20)
 outputflename.grid(column=1,row=2, padx=5,pady=5)
 
-splitterlabel = tk.Label(totalframe, text="Splitter Tag",fg="Black",font=('Helvetica','12'),bg="white")
+splitterlabel = tk.Label(splittab, text="Splitter Tag",fg="Black",font=('Helvetica','12'),bg="white")
 splitterlabel.grid(column=0,row=3, padx=5,pady=5,sticky = W)
 
-splittertag = tk.Entry(totalframe,fg="Black",font=('Helvetica','12'),bg="white",width=10)
+splittertag = tk.Entry(splittab,fg="Black",font=('Helvetica','12'),bg="white",width=10)
 splittertag.insert(END,'employee')
 splittertag.grid(column=1,row=3, padx=5,pady=5,sticky = W)
 
 
-trimbutton = tk.Button(totalframe,text="SPLIT",command=split, relief=RAISED,fg="Black",font=('Helvetica','14','bold'))
+trimbutton = tk.Button(splittab,text="SPLIT",command=split, relief=RAISED,fg="Black",font=('Helvetica','14','bold'))
 trimbutton.place(relx=0.3,rely=0.65,relwidth=0.2,relheight=0.1,anchor='n')
+
+#Trim tab controls
+
+
+# trim function splits the whole xml file into child files having 10,000 records of the tag mentioned
+def trim():
+    #validate
+    messagelist=[]
+    if(trimfilename.get() == ''):
+        messagelist.append(validations[0])
+    if(trimsplittertag.get()== ''):
+        messagelist.append(validations[2])
+
+    if(len(messagelist)== 0):        
+        splittag=str(trimsplittertag.get()) 
+        iterator = str(trimiteratortag.get())       
+        splitxmlfilewithcounter(trimfilepath,splittag,iterator)
+        messagebox.showinfo('Success',f'Files generated in the path: {os.getcwd()+"/Output"+"/"}')
+    else:
+        messages = ''
+        for message in messagelist:
+            messages = messages + message +"\n"
+        messagebox.showinfo('Warning',messages)
+
+trimfilepath =''
+def trimbrowse():    
+    global trimfilepath
+    trimfilepath = filedialog.askopenfilename(
+    title="Choose directory",
+    initialdir=os.getcwd(),
+    filetypes=[("XML Files", "*.xml")]  
+    )   
+    trimfilename.delete(0,END) 
+    trimfilename.insert(END,Path(trimfilepath).name)
+
+trimbrowselabel = tk.Label(trimtab, text="Please select the XML File *",fg="Black",font=('Helvetica','12'),bg="white")
+trimbrowselabel.grid(column=0,row=0, padx=5,pady=5,sticky = W)
+
+trimfilename = tk.Entry(trimtab,fg="Black",font=('Helvetica','12'),bg="white",width=20)
+trimfilename.grid(column=1,row=0, padx=5,pady=5)
+
+trimbrowsebutton = tk.Button(trimtab,text='Browse', command=trimbrowse,fg="Black",font=('Helvetica','12'))
+trimbrowsebutton.grid(column=2,row=0,padx=5,pady=5)
+
+trimsplitterlabel = tk.Label(trimtab, text="Splitter Tag",fg="Black",font=('Helvetica','12'),bg="white")
+trimsplitterlabel.grid(column=0,row=1, padx=5,pady=5,sticky = W)
+
+trimsplittertag = tk.Entry(trimtab,fg="Black",font=('Helvetica','12'),bg="white",width=10)
+trimsplittertag.insert(END,'employee')
+trimsplittertag.grid(column=1,row=1, padx=5,pady=5,sticky = W)
+
+
+trimiteratorlabel = tk.Label(trimtab, text="Iterator",fg="Black",font=('Helvetica','12'),bg="white")
+trimiteratorlabel.grid(column=0,row=2, padx=5,pady=5,sticky = W)
+
+trimiteratortag = tk.Entry(trimtab,fg="Black",font=('Helvetica','12'),bg="white",width=10)
+trimiteratortag.insert(END,'10000')
+trimiteratortag.grid(column=1,row=2, padx=5,pady=5,sticky = W)
+
+
+removecontents = tk.Button(trimtab,text='TRIM', command=trim, relief=RAISED,fg="Black",font=('Helvetica','14','bold'))
+removecontents.place(relx=0.5,rely=0.65,relwidth=0.2,relheight=0.1,anchor='n')
 
 
 #Total count tab controls
+countfilepath = ''
+
+def totalbrowse():    
+    global countfilepath
+    countfilepath = filedialog.askopenfilename(
+    title="Choose directory",
+    initialdir=os.getcwd(),
+    filetypes=[("XML Files", "*.xml")]  
+    )   
+    totalbrowsefilename.delete(0,END) 
+    totalbrowsefilename.insert(END,Path(countfilepath).name)
+
+def totalcounting():
+    checkcounter = os.path.exists(countfilepath)
+    if(not checkcounter):
+        messagebox.showinfo('Validation Error',"Please provide a valid file imput file")
+    count = elementcount(countfilepath,str(totalsplittertag.get()))
+    totalcountvalue.delete(0,END)
+    totalcountvalue.insert(END,count)
+
 
 totalbrowselabel = tk.Label(totalcounttab, text="Please select the XML File *",fg="Black",font=('Helvetica','12'),bg="white")
 totalbrowselabel.grid(column=0,row=0, padx=5,pady=5,sticky = W)
@@ -191,19 +252,24 @@ totalbrowselabel.grid(column=0,row=0, padx=5,pady=5,sticky = W)
 totalbrowsefilename = tk.Entry(totalcounttab,fg="Black",font=('Helvetica','12'),bg="white",width=20)
 totalbrowsefilename.grid(column=1,row=0, padx=5,pady=5)
 
-totalbrowsebutton = tk.Button(totalcounttab,text='Browse', command=browse,fg="Black",font=('Helvetica','12'))
+totalbrowsebutton = tk.Button(totalcounttab,text='Browse', command=totalbrowse,fg="Black",font=('Helvetica','12'))
 totalbrowsebutton.grid(column=2,row=0,padx=5,pady=5)
 
+totalsplitterlabel = tk.Label(totalcounttab, text="Splitter Tag",fg="Black",font=('Helvetica','12'),bg="white")
+totalsplitterlabel.grid(column=0,row=1, padx=5,pady=5,sticky = W)
 
-totalcountbutton = tk.Button(totalcounttab, text="Total",command=counting,relief=RAISED,fg="Black",font=('Helvetica','14','bold'))
-totalcountbutton.grid(column=0,row=1, padx=5,pady=5,sticky = W)
+totalsplittertag = tk.Entry(totalcounttab,fg="Black",font=('Helvetica','12'),bg="white",width=10)
+totalsplittertag.insert(END,'employee')
+totalsplittertag.grid(column=1,row=1, padx=5,pady=5,sticky = W)
+
+
+totalcountbutton = tk.Button(totalcounttab, text="Total",command=totalcounting,relief=RAISED,fg="Black",font=('Helvetica','14','bold'))
+totalcountbutton.grid(column=0,row=2, padx=5,pady=5,sticky = W)
 
 totalcountvalue = tk.Entry(totalcounttab, relief=RAISED,fg="Black",font=('Helvetica','14','bold'))
-totalcountvalue.grid(column=1,row=1, padx=5,pady=5,sticky = W)
+totalcountvalue.grid(column=1,row=2, padx=5,pady=5,sticky = W)
 
 
-#removecontents = tk.Button(baseframe,text='Split', command=split, relief=RAISED,fg="Black",font=('Helvetica','14','bold'))
-#removecontents.place(relx=0.5,rely=0.65,relwidth=0.2,relheight=0.1,anchor='n')
 
 #searchandtrim = tk.Button(baseframe,text='Search&Trim', command=searchandtrim, relief=RAISED,fg="Black",font=('Helvetica','14','bold'))
 #searchandtrim.place(relx=0.75,rely=0.65,relwidth=0.2,relheight=0.1,anchor='n')
